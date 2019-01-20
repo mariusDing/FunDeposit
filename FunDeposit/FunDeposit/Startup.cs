0903be98710bf48d37be8216aad7c7407a20ada6
MariusDing <xuanding0906@gmail.com>
2019-01-21 02:59:57 +1100

    Clear up unnesscessary controller and view

--- FunDeposit/FunDeposit/Startup.cs
+++ FunDeposit/FunDeposit/Startup.cs
@@ -76,7 +76,7 @@ namespace FunDeposit
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Deposit}/{action=Index}/{id?}");
             });
         }
     }
